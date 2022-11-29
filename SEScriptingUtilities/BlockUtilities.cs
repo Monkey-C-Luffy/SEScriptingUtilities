402fb566d066468c371d6918383f0a44ab8731a5
Vasileps <nfsps@msn.com>
2022-11-29 16:38:08 +0200

    Added BlockUtilties for any BlockUtilities needed.

---
+++ SEScriptingUtilities/BlockUtilities.cs
@@ -0,0 +1,16 @@
+﻿/*
+  MIT License
+
+Copyright (c) 2022 Monkey C Luffy
+ */
+using System;
+using System.Collections.Generic;
+using Sandbox.ModAPI.Ingame;
+
+namespace SEScripting
+{
+    public static class BlockUtilities
+    {
+
+    }
+}
