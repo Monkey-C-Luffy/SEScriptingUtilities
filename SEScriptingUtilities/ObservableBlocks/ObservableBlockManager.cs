56ddf41c66cab76f17c97becd11549e67ea6d045
When PC <nfsps@msn.com>
2023-10-22 16:54:09 +0300

    Attempting to make ObservableBlockmanager

---
+++ SEScriptingUtilities/ObservableBlocks/ObservableBlockManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEScriptingUtilities
+{
+    public class ObservableBlockManager
+    {
+        List<ObservableBlock> observableBlocks = new List<ObservableBlock>();
+    }
+}
